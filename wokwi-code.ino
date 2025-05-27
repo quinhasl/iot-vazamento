3c42f50268c2178d3e06c84358d99b46ec8490c3
Lucas Fernandes <155379823+quinhasl@users.noreply.github.com>
2025-05-27 20:47:28 -0300

    Update wokwi-code.ino

--- wokwi-code.ino
+++ wokwi-code.ino
@@ -12,74 +12,36 @@ PubSubClient client(espClient);
 #define BOTAO_FLUXO 2
 #define BUZZER 3
 
-unsigned long lastMsgTime = 0;
-const long interval = 1000;
-
 void setup() {
   Serial.begin(115200);
-  
   pinMode(BOTAO_FLUXO, INPUT_PULLUP);
   pinMode(BUZZER, OUTPUT);
   
-  WiFi.begin(ssid, password);
-  Serial.print("Conectando ao WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConectado!");
+  Serial.println("Conectando ao WiFi...");
+  delay(1000);
+  Serial.println("Conectado! IP: 192.168.1.100 (simulado)");
   
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  Serial.println("Configurando MQTT...");
+  Serial.println("Broker: broker.hivemq.com:1883");
+  Serial.println("Tópico: projeto-vazamento");
+  Serial.println("----------------------------------");
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
   int umidade = analogRead(SENSOR_UMIDADE);
   int fluxo = digitalRead(BOTAO_FLUXO);
 
   if (umidade < 2000 || fluxo == LOW) {
-    digitalWrite(BUZZER, HIGH);
+    digitalWrite(BUZZER, HIGH);  
     
-    unsigned long now = millis();
-    if (now - lastMsgTime > interval) {
-      lastMsgTime = now;
-      if (client.publish("projeto-vazamento", "ALERTA: VAZAMENTO DETECTADO!")) {
-        Serial.println("Mensagem MQTT enviada!");
-      } else {
-        Serial.println("Falha no envio MQTT");
-      }
-    }
+    Serial.println("[MQTT SIMULADO] Mensagem enviada para broker.hivemq.com:");
+    Serial.println("Tópico: projeto-vazamento");
+    Serial.println("Payload: {\"umidade\":" + String(umidade) + ", \"alerta\":\"VAZAMENTO DETECTADO\"}");
+    Serial.println("----------------------------------");
+    
+    delay(3000);  
   } else {
     digitalWrite(BUZZER, LOW);
-  }
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensagem recebida [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Conectando ao MQTT...");
-    if (client.connect("ESP32-Wokwi")) {
-      Serial.println("Conectado!");
-      client.subscribe("projeto-vazamento");
-    } else {
-      Serial.print("Falha, rc=");
-      Serial.print(client.state());
-      Serial.println(" Tentando novamente em 5 segundos...");
-      delay(5000);
-    }
   }
 }
